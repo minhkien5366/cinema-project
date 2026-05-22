c64fe0a35bc581b874fcd3fa25b9b696289c5c00
Huynh Thi Ngoc Anh <huynhthingocanh2008@gmail.com>
2026-05-22 16:10:33 +0700

    update cinema feature

--- src/main/java/com/example/cinema/entity/Genre.java
+++ src/main/java/com/example/cinema/entity/Genre.java
@@ -1,9 +1,9 @@
 package com.example.cinema.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.ToString;
+
 import java.util.HashSet;
 import java.util.Set;
 
@@ -11,19 +11,16 @@ import java.util.Set;
 @Table(name = "genres")
 @Data
 public class Genre {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    @Column(unique = true)
     private String name;
 
-    @Column(columnDefinition = "TEXT")
     private String description;
 
-    // 🎯 THÊM MỚI: Mối quan hệ đảo chiều từ Genre sang Movie
-    @ManyToMany(mappedBy = "genres", fetch = FetchType.LAZY)
-    @ToString.Exclude          // Tránh vòng lặp vô hạn lombok
-    @EqualsAndHashCode.Exclude // Tránh vòng lặp vô hạn lombok
+    @ManyToMany(mappedBy = "genres")
+    @JsonIgnore
     private Set<Movie> movies = new HashSet<>();
 }
