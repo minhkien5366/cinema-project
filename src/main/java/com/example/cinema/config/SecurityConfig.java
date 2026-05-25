aac99ce8c7e2ee727a30255d24bfe5fcf0c57642
Huynh Thi Ngoc Anh <huynhthingocanh2008@gmail.com>
2026-05-25 21:02:01 +0700

    update cinema feature

--- src/main/java/com/example/cinema/config/SecurityConfig.java
+++ src/main/java/com/example/cinema/config/SecurityConfig.java
@@ -81,7 +81,7 @@ public class SecurityConfig {
         CorsConfiguration configuration = new CorsConfiguration();
         
         // FIX: Cho phép cả localhost và 127.0.0.1 để tránh xung đột cổng gọi của trình duyệt
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://127.0.0.1:3000")); 
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://127.0.0.1:3000","https://akcinema.vercel.app/")); 
         
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "Accept", "X-Requested-With"));
--- src/main/java/com/example/cinema/config/WebConfig.java
+++ src/main/java/com/example/cinema/config/WebConfig.java
@@ -8,7 +8,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        // Ánh xạ URL /uploads/movies/** vào thư mục vật lý uploads/movies/
         registry.addResourceHandler("/uploads/**")
                 .addResourceLocations("file:uploads/");
     }
